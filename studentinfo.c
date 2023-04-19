0f5c594e1c21561344c2b9ddf25f14e4d357f099
krishna684 <50433347+krishna684@users.noreply.github.com>
2023-04-19 06:32:45 +0530

    Update studentinfo.c

--- studentinfo.c
+++ studentinfo.c
@@ -1,19 +1,22 @@
 #include <stdio.h> 
-Struct Student 
+
+struct Student 
 { 
-int rno; 
-char name [25]; 
-int m, P, C; 
-}std; 
-void main () 
+    int rno; 
+    char name[25]; 
+    int m, p, c; 
+} std; 
+
+int main() 
 { 
-  float tot, avg;
-  printf ("enter details \n"); 
-  scanf ("%d %s %d%d %d",&std.rno,&std.name,&std.m,&std.p&std.c);
-  tot = std.m+std.p+std.c;
-  avg = tot/3;
-  printf ("rollNo = %d \n",std.rno);
-  printf ("name = %s In",std.name);
-  printf ("total = %f\n",tot);
-  printf ("average = %f \n", avg);
+    float tot, avg;
+    printf("Enter details:\n"); 
+    scanf("%d %s %d %d %d", &std.rno, std.name, &std.m, &std.p, &std.c);
+    tot = std.m + std.p + std.c;
+    avg = tot / 3;
+    printf("Roll No = %d \n", std.rno);
+    printf("Name = %s\n", std.name);
+    printf("Total = %f\n", tot);
+    printf("Average = %f\n", avg);
+    return 0;
 }
