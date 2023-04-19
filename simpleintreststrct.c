58979fb17e3025a2ac0618b0d532b4f242ec3c70
krishna684 <50433347+krishna684@users.noreply.github.com>
2023-04-19 06:22:26 +0530

    Create simpleintreststrct.c

---
+++ simpleintreststrct.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+Struct simple intrest
+{ float P;
+float n;
+float r;
+} 
+struct Simple intrest SI;
+void main () 
+{
+  float result; 
+  printf ("Enter P,n,r");
+  Scanf("%f%f%f", & SI.P, & SI.n,SI.r).\;
+  result= SI-P*SI.n*SI·r/100; 
+    printf ("simple result =%f",result); 
+   return 0; 
+}
